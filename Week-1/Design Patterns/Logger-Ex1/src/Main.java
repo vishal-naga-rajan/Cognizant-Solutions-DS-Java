209a158c9c56d36ebb8a83e223d69029ca375995
vishal-naga-rajan <vishalnagarajane@gmail.com>
2025-06-20 13:19:54 +0530

    Logger Program

---
+++ Week-1/Design Patterns/Logger-Ex1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("This is the first log.");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("This is the second log.");
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same.");
+        } else {
+            System.out.println("Logger instances are different.");
+        }
+    }
+}
