61506b1146921b668b4aee4d2fb75c9bfe0d3968
vishal-naga-rajan <vishalnagarajane@gmail.com>
2025-07-20 22:09:24 +0530

    Final Commit for Week - 5

---
+++ Week-5/22022222/Microservices/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,22 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Map<String, Object> getLoan(@PathVariable String number) {
+        Map<String, Object> loan = new HashMap<>();
+        loan.put("number", number);
+        loan.put("type", "car");
+        loan.put("loan", 400000);
+        loan.put("emi", 3258);
+        loan.put("tenure", 18);
+        return loan;
+    }
+}
